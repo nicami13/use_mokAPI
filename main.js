18f2babc85013d692e3bdf30ca0450a960c8e230
undefain <137202367+nicami13@users.noreply.github.com>
2023-09-02 00:33:09 -0500

    feat:✨:Metodo obtenerObjetos
    A method was implemented to bring the future objects that will be given by the rest of the team in order to structure the invoice.

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ document.getElementById("miFormulario").addEventListener("submit", function(even
         cliente: cliente,
         total: total
     };
-    console.log(obj)
     enviarDatos(obj);
 });
 
@@ -30,4 +29,29 @@ async function enviarDatos(data) {
         console.error(error);
     }
 }
+
+async function obtenerObjetos() {
+    try {
+      // Realiza una solicitud GET al servidor para obtener objetos
+      const response = await fetch('https://64f2031d0e1e60602d2492a7.mockapi.io/usuario1');
+  
+      // Verifica si la respuesta es exitosa (código de estado HTTP 200-299)
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+  
+      // Analiza el cuerpo de la respuesta como JSON y almacena los objetos en una variable
+      const objetos = await response.json();
+  
+      // Procesa los objetos obtenidos de la respuesta
+      console.log(objetos);
+    } catch (error) {
+      // Maneja cualquier error que ocurra durante la solicitud o el análisis JSON
+      console.error(error);
+    }
+  }
+  
+  // Llama al método para obtener objetos del servidor
+  obtenerObjetos();
+  
 
