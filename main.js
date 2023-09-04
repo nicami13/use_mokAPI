af42b6f0993a419c98b722b143745142020b2a5c
undefain <137202367+nicami13@users.noreply.github.com>
2023-09-04 18:18:39 -0500

    feat: 👷‍♂️: install the new functions
    Install the new functions  use the css files and new forms for conect the mokapi

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ document.getElementById("miFormulario").addEventListener("submit", function(even
     event.preventDefault();
 
     let ref = document.getElementById("ref").value;
-    let productos = document.getElementById("productos").value;
-    let cliente = document.getElementById("cliente").value;
+    let productos = document.getElementByName("productos").value;
+    let cliente = document.getElementByName("cliente").value;
     let total = parseFloat(document.getElementById("total").value);
 
     let obj = {
@@ -54,4 +54,43 @@ async function obtenerObjetos() {
   // Llama al método para obtener objetos del servidor
   obtenerObjetos();
   
-
+async function obtenerObjetosCliente() {
+    try {
+      // Realiza una solicitud GET al servidor para obtener objetos
+      const response = await fetch('https://64f64cd42b07270f705e5fb7.mockapi.io/cliente');
+  
+      // Verifica si la respuesta es exitosa (código de estado HTTP 200-299)
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+  
+      // Analiza el cuerpo de la respuesta como JSON y almacena los objetos en una variable
+      const objetos = await response.json();
+  
+      // Procesa los objetos obtenidos de la respuesta
+      console.log(objetos);
+    } catch (error) {
+      // Maneja cualquier error que ocurra durante la solicitud o el análisis JSON
+      console.error(error);
+    }
+  }
+  async function obtenerObjetosCliente() {
+    try {
+      // Realiza una solicitud GET al servidor para obtener objetos
+      const response = await fetch('https://64f64d8d2b07270f705e6011.mockapi.io/producto');
+  
+      // Verifica si la respuesta es exitosa (código de estado HTTP 200-299)
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+  
+      // Analiza el cuerpo de la respuesta como JSON y almacena los objetos en una variable
+      const objetos = await response.json();
+  
+      // Procesa los objetos obtenidos de la respuesta
+      console.log(objetos);
+    } catch (error) {
+      // Maneja cualquier error que ocurra durante la solicitud o el análisis JSON
+      console.error(error);
+    }
+  }
---
+++ producto.js
@@ -0,0 +1,53 @@
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let ref = document.getElementById("ref").value;
+    let productos = document.getElementById("productos").value;
+    let cliente = document.getElementById("cliente").value;
+
+    let obj = {
+        identificador: ref,
+        nombre: productos,
+        descripcion: cliente
+    };
+    enviarDatos(obj);
+});
+
+async function enviarDatos(data) {
+    try {
+        let config = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        };
+        let peticion = await fetch('https://64f64d8d2b07270f705e6011.mockapi.io/producto', config);
+        let res = await peticion.json();
+        console.log(res);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function obtenerObjetos() {
+    try {
+      // Realiza una solicitud GET al servidor para obtener objetos
+      const response = await fetch('https://64f64d8d2b07270f705e6011.mockapi.io/producto');
+  
+      // Verifica si la respuesta es exitosa (código de estado HTTP 200-299)
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+  
+      // Analiza el cuerpo de la respuesta como JSON y almacena los objetos en una variable
+      const objetos = await response.json();
+  
+      // Procesa los objetos obtenidos de la respuesta
+      console.log(objetos);
+    } catch (error) {
+      // Maneja cualquier error que ocurra durante la solicitud o el análisis JSON
+      console.error(error);
+    }
+  }
+  
+  // Llama al método para obtener objetos del servidor
+  obtenerObjetos();
